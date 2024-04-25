4d70124eb0967b24af45503440081ca3c30d3ffc
notzith <mkvishvajith@gmail.com>
2024-04-25 17:40:59 +0530

    intial

---
+++ com/yourcompany/permutationviz/Permutation.java
@@ -0,0 +1,32 @@
+package com.yourcompany.permutationviz;
+
+public class Permutation  {
+    static Visual v;
+    public static void main(String[] args) {
+ 
+        String s="abcd";
+        v= new Visual(s);
+        v.insEle(s,0);
+        display(s,0,s.length()-1);
+        v.disMatrix(v.triangle);
+    }
+
+    static void display(String s,int start,int end) {
+        if(start==end) {
+            //System.out.println(s);
+            return;
+        }
+        for(int i=start; i<=end; i++) {
+            String s1 = swap(s,start,i);
+            v.insEle(s1,start+1);
+            display(s1,start+1,end);
+        }
+    }
+    static String swap(String s,int i,int j) {
+        char[]a=s.toCharArray();
+        char t = a[i];
+        a[i]=a[j];
+        a[j]=t;
+        return new String(a);
+    }
+}
---
+++ com/yourcompany/permutationviz/Visual.java
@@ -0,0 +1,62 @@
+package com.yourcompany.permutationviz;
+
+import java.util.Arrays;
+
+public class Visual {
+    public String[][] triangle;
+    int count,strlen;
+    Visual(String str){
+        triangleMaker(str);
+        strlen=str.length();
+    }
+    void insEle(String s,int i){
+        if(count==0){
+            triangle[0][0]=s;
+            count=1;
+        }
+        autoFill(triangle[i], s);
+        disMatrix(triangle);
+    }
+    void autoFill(String[] sarr,String str){
+        for(int i=0 ;i<sarr.length;i++){
+            if (sarr[i]==null){
+                sarr[i]=str;
+                break;
+            }
+        }
+    }
+
+    private void triangleMaker(String s){
+        int i= s.length();
+        triangle = new String[i][];
+        triangle[0]=new String[1];
+        int fact=i,j=1,temp=i;
+        while(j<i){
+            triangle[j++]=new String[fact];
+            fact*=(--temp);
+        }
+        disMatrix(triangle);
+    }
+    void disMatrix(String[][] matrix) {
+        int spaces=strlen*2;
+        for(String[] s : matrix){
+            for(int i=0;i<spaces;i++)System.out.print("\t");
+            System.out.println(Arrays.toString(s));
+            spaces-=3;
+        }
+    }
+
+
+}
+
+//        for(int i =0;i<matrix.length;i++){
+//            for(int j=0;j<spaces;j--){
+//                System.out.print("\t");
+//            }
+//            for(int j=0; j<matrix[i].length;j++){
+//                System.out.print(matrix[i][j]+"  ");
+//            }
+//            System.out.println();
+//            count-=2;
+//        }
+
